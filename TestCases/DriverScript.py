a4219022ae76d9da5e502b4cdf88a3d8ba325e56
nivinselvam <nivin.p@ezetap.com>
2022-05-16 18:31:02 +0530

    Handling none type for getDevicesList issue fixed

--- TestCases/DriverScript.py
+++ TestCases/DriverScript.py
@@ -7,33 +7,25 @@ from Utilities import Rerun
 from Utilities import ConfigReader
 from Configuration import TestSuiteSetup
 
-# ResourceAssigner.clearAssignerTables()
-# TestSuiteSetup.startEmulators(TestSuiteSetup.getThreadCount())
-# #time.sleep(15)
-# devices = TestSuiteSetup.getDevicesList()
-# appium_server_ports = TestSuiteSetup.startAppiumServers(len(devices))
-# # # users = [{"Username":"7204644777","Password":"A123456"},{"Username":"7204644333","Password":"A123456"},{"Username":"7204644666","Password":"A123456"}]
-# ResourceAssigner.updateDevicesInDB(devices)
-# ResourceAssigner.updateAppiumServersInDB(appium_server_ports)
-# # ResourceAssigner.updateUsersInDB(users)
-TestSuiteSetup.prepareDevicesAndDB()
 
-TestSuiteSetup.prepare_Consolidated_List_Of_TestcasesFile()
+if TestSuiteSetup.prepareDevicesAndDB():
 
-if ConfigReader.read_config("Validations", "bool_rerun_immediately").lower() == "true" and ConfigReader.read_config("Validations", "bool_rerun_at_the_end").lower() == "false":
-    Rerun.prepareImmediateRerunExcel()
+    TestSuiteSetup.prepare_Consolidated_List_Of_TestcasesFile()
 
-TestSuiteSetup.executeSelectedTestCases()
+    if ConfigReader.read_config("Validations", "bool_rerun_immediately").lower() == "true" and ConfigReader.read_config("Validations", "bool_rerun_at_the_end").lower() == "false":
+        Rerun.prepareImmediateRerunExcel()
 
-# Rerun
-rerunCount = int(ConfigReader.read_config("Validations", "int_rerun_count"))
+    TestSuiteSetup.executeSelectedTestCases()
 
-if ConfigReader.read_config("Validations", "bool_rerun_at_the_end").lower() == "true":
-    Rerun.prepareAtTheEndRerunExcel()
-    while rerunCount > 0:
-        rerunCount = rerunCount - 1
-        Rerun.setRerunCount("",rerunCount)
-        Rerun.rerunTestAtTheEnd()
-    Rerun.prepareAtTheEndRerunExcel()
+    # Rerun
+    rerunCount = int(ConfigReader.read_config("Validations", "int_rerun_count"))
+
+    if ConfigReader.read_config("Validations", "bool_rerun_at_the_end").lower() == "true":
+        Rerun.prepareAtTheEndRerunExcel()
+        while rerunCount > 0:
+            rerunCount = rerunCount - 1
+            Rerun.setRerunCount("",rerunCount)
+            Rerun.rerunTestAtTheEnd()
+        Rerun.prepareAtTheEndRerunExcel()
 
 TestSuiteSetup.killAppiumServers()
