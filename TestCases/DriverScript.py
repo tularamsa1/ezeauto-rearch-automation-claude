4e7147a99cabd30454ce6ade51e92d437327c1f4
nivinselvam <nivin.p@ezetap.com>
2022-05-20 21:28:52 +0530

    Desired capabilities updated, Time formatting issue fixed.

---
+++ TestCases/DriverScript.py
@@ -0,0 +1,28 @@
+from Utilities import DirectoryCreator
+DirectoryCreator.createExecutionDirectories()
+from Utilities import Rerun
+from Utilities import ConfigReader
+from Configuration import TestSuiteSetup
+
+
+if TestSuiteSetup.prepareDevicesAndDB():
+
+    TestSuiteSetup.prepare_Consolidated_List_Of_TestcasesFile()
+
+    if ConfigReader.read_config("Validations", "bool_rerun_immediately").lower() == "true" and ConfigReader.read_config("Validations", "bool_rerun_at_the_end").lower() == "false":
+        Rerun.prepareImmediateRerunExcel()
+
+    TestSuiteSetup.executeSelectedTestCases()
+
+    # Rerun
+    rerunCount = int(ConfigReader.read_config("Validations", "int_rerun_count"))
+
+    if ConfigReader.read_config("Validations", "bool_rerun_at_the_end").lower() == "true":
+        Rerun.prepareAtTheEndRerunExcel()
+        while rerunCount > 0:
+            rerunCount = rerunCount - 1
+            Rerun.setRerunCount("", rerunCount)
+            Rerun.rerunTestAtTheEnd()
+        Rerun.prepareAtTheEndRerunExcel()
+
+TestSuiteSetup.killEmulatorsAndAppiumServers()
