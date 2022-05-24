ba70573a541bec1cf09fc35fa769dc7217644b97
nivinselvam <nivin.p@ezetap.com>
2022-05-24 12:59:13 +0530

    Resource assigner enhanced to handle app user and portal users

--- TestCases/DriverScript.py
+++ TestCases/DriverScript.py
@@ -4,25 +4,24 @@ from Utilities import Rerun
 from Utilities import ConfigReader
 from Configuration import TestSuiteSetup
 
-
-if TestSuiteSetup.prepareDevicesAndDB():
-
-    TestSuiteSetup.prepare_Consolidated_List_Of_TestcasesFile()
-
-    if ConfigReader.read_config("Validations", "bool_rerun_immediately").lower() == "true" and ConfigReader.read_config("Validations", "bool_rerun_at_the_end").lower() == "false":
-        Rerun.prepareImmediateRerunExcel()
-
-    TestSuiteSetup.executeSelectedTestCases()
-
-    # Rerun
-    rerunCount = int(ConfigReader.read_config("Validations", "int_rerun_count"))
-
-    if ConfigReader.read_config("Validations", "bool_rerun_at_the_end").lower() == "true":
-        Rerun.prepareAtTheEndRerunExcel()
-        while rerunCount > 0:
-            rerunCount = rerunCount - 1
-            Rerun.setRerunCount("", rerunCount)
-            Rerun.rerunTestAtTheEnd()
-        Rerun.prepareAtTheEndRerunExcel()
-
-TestSuiteSetup.killEmulatorsAndAppiumServers()
+try:
+    if TestSuiteSetup.prepareDevicesAndDB():
+        TestSuiteSetup.prepare_Consolidated_List_Of_TestcasesFile()
+        if ConfigReader.read_config("Validations", "bool_rerun_immediately").lower() == "true" and ConfigReader.read_config("Validations", "bool_rerun_at_the_end").lower() == "false":
+            Rerun.prepareImmediateRerunExcel()
+        else:
+            print("Immediate rerun is disabled for this execution.")
+        TestSuiteSetup.executeSelectedTestCases()
+        # Rerun
+        if ConfigReader.read_config("Validations", "bool_rerun_at_the_end").lower() == "true":
+            rerunCount = int(ConfigReader.read_config("Validations", "int_rerun_count"))
+            Rerun.prepareAtTheEndRerunExcel()
+            while rerunCount > 0:
+                rerunCount = rerunCount - 1
+                Rerun.setRerunCount("", rerunCount)
+                Rerun.rerunTestAtTheEnd()
+            Rerun.prepareAtTheEndRerunExcel()
+    else:
+        print("Issue in preparing the devices and automation db for execution. Hence could not start execution.")
+finally:
+    TestSuiteSetup.killEmulatorsAndAppiumServers()
