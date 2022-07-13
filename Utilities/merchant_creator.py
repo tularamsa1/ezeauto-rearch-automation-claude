5fdf2341b19fe83d46394858718275aaeb8814d3
nivinselvam <nivin.p@ezetap.com>
2022-07-13 15:16:10 +0530

    Excel implementation for merchant creation completed.

--- Utilities/merchant_creator.py
+++ Utilities/merchant_creator.py
@@ -1,13 +1,100 @@
 import requests
 import json
+import pandas
 import sqlite3
 from Utilities import DBProcessor, ConfigReader
 from DataProvider import GlobalConstants
 from Utilities.execution_log_processor import EzeAutoLogger
 
-logger = EzeAutoLogger(__name__)
 
+logger = EzeAutoLogger(__name__)
 dbPath = ConfigReader.read_config_paths("System", "automation_suite_path")+"/Database/ezeauto.db"
+excel_path = ConfigReader.read_config_paths("System", "automation_suite_path")+"/DataProvider/merchant_user_creation.xlsx"
+
+
+
+def create_merchants_with_users():
+    """
+    This method is used to create the merchants along with the users.
+    This calls the generate_merchant_creation_api_body method for getting the list of request bodies.
+    Each request body represents a merchant.
+    """
+    api_details = get_api_from_db("createMerchant")
+    conn = sqlite3.connect(dbPath)
+    cursor = conn.cursor()
+    url = ConfigReader.read_config("APIs", "baseurl")+api_details["EndPoint"]
+    headers = json.loads(api_details["Header"])
+    global user_name
+    lst_merchant_creation_body = generate_merchant_creation_api_body()
+    if lst_merchant_creation_body:
+        for merchant_creation_api_body in lst_merchant_creation_body:
+            user_name = merchant_creation_api_body["merchantCode"]
+            payload = json.dumps(merchant_creation_api_body)
+            logger.debug(payload)
+            response = requests.request("POST", url, headers=headers, data=payload)
+            logger.debug(response.text)
+            result = json.loads(response.text)
+            if result["success"]:
+                logger.info(f"Merchant {user_name} with users created successfully.")
+                try:
+                    cursor.execute("update merchants set CreationStatus = 'Created', Availability = 'Available' where MerchantCode = '" + user_name + "'")
+                    cursor.execute("update users set CreationStatus = 'Created', Availability = 'Available' where MerchantCode = '" + user_name + "'")
+                    conn.commit()
+                except Exception as e:
+                    print(f"Unable to update the ezeauto db on the merchant {user_name} creation due to error " + str(e))
+                    logger.warning(f"Unable to update the ezeauto db on the merchant {user_name} creation due to error " + str(e))
+            else:
+                logger.warning(f"Merchant {user_name} creation failed")
+
+    else:
+        print("Merchant creation skipped.")
+        logger.info("Merchant creation skipped")
+    cursor.close()
+    conn.close()
+
+
+def create_users_for_merchant(merchant_code):
+    """
+        This method is used to create the users for the specified merchant
+        This calls the generate_user_creation_api_body method for getting the list of request bodies.
+        Each request body represents a user.
+        """
+    api_details = get_api_from_db("createUser")
+    conn = sqlite3.connect(dbPath)
+    cursor = conn.cursor()
+    url = ConfigReader.read_config("APIs", "baseurl") + api_details["EndPoint"]
+    headers = json.loads(api_details["Header"])
+    global user_name
+    lst_user_creation_body = generate_users_creation_api_body(merchant_code)
+    if lst_user_creation_body:
+        for user_creation_api_body in lst_user_creation_body:
+            user_name = user_creation_api_body["name"]
+            payload = json.dumps(user_creation_api_body)
+            logger.debug(payload)
+            response = requests.request("POST", url, headers=headers, data=payload)
+            logger.debug(response.text)
+            result = json.loads(response.text)
+            if result["success"]:
+                logger.info(f"User {user_name} with users created successfully.")
+                try:
+                    cursor.execute(
+                        "update users set CreationStatus = 'Created', Availability = 'Available' where Name = '" + user_name + "'")
+                    conn.commit()
+                except Exception as e:
+                    print(
+                        f"Unable to update the ezeauto db on the user {user_name} creation due to error " + str(
+                            e))
+                    logger.warning(
+                        f"Unable to update the ezeauto db on the user {user_name} creation due to error " + str(
+                            e))
+            else:
+                logger.warning(f"User {user_name} creation failed")
+
+    else:
+        print("User creation skipped.")
+        logger.info("User creation skipped")
+    cursor.close()
+    conn.close()
 
 
 def get_api_from_db(api_name:str) -> dict:
@@ -53,6 +140,7 @@ def check_if_merchant_exists(merchant_code: str) -> bool:
         print("Merchant " + merchant_code + " already exists in this environment")
         logger.info("Merchant " + merchant_code + " already exists in this environment")
         return True
+
 
 def check_if_user_exists(name: str) -> bool:
     """
@@ -138,46 +226,6 @@ def generate_merchant_creation_api_body() -> list:
     return lst_merchant_creation_api_body
 
 
-def create_merchants_with_users():
-    """
-    This method is used to create the merchants along with the users.
-    This calls the generate_merchant_creation_api_body method for getting the list of request bodies.
-    Each request body represents a merchant.
-    """
-    api_details = get_api_from_db("createMerchant")
-    conn = sqlite3.connect(dbPath)
-    cursor = conn.cursor()
-    url = ConfigReader.read_config("APIs", "baseurl")+api_details["EndPoint"]
-    headers = json.loads(api_details["Header"])
-    global user_name
-    lst_merchant_creation_body = generate_merchant_creation_api_body()
-    if lst_merchant_creation_body:
-        for merchant_creation_api_body in lst_merchant_creation_body:
-            user_name = merchant_creation_api_body["merchantCode"]
-            payload = json.dumps(merchant_creation_api_body)
-            logger.debug(payload)
-            response = requests.request("POST", url, headers=headers, data=payload)
-            logger.debug(response.text)
-            result = json.loads(response.text)
-            if result["success"]:
-                logger.info(f"Merchant {user_name} with users created successfully.")
-                try:
-                    cursor.execute("update merchants set CreationStatus = 'Created', Availability = 'Available' where MerchantCode = '" + user_name + "'")
-                    cursor.execute("update users set CreationStatus = 'Created', Availability = 'Available' where MerchantCode = '" + user_name + "'")
-                    conn.commit()
-                except Exception as e:
-                    print(f"Unable to update the ezeauto db on the merchant {user_name} creation due to error " + str(e))
-                    logger.warning(f"Unable to update the ezeauto db on the merchant {user_name} creation due to error " + str(e))
-            else:
-                logger.warning(f"Merchant {user_name} creation failed")
-
-    else:
-        print("Merchant creation skipped.")
-        logger.info("Merchant creation skipped")
-    cursor.close()
-    conn.close()
-
-
 def generate_users_creation_api_body(merchant_code:str) -> list:
     """
     This method is used to generate the request body of the user creation API.
@@ -231,45 +279,112 @@ def generate_users_creation_api_body(merchant_code:str) -> list:
         logger.fatal("Unable to fetch user creation details from the ezeauto.db due to error "+str(e))
     return lst_user_creation_api_body
 
-def create_users_for_merchant(merchant_code):
-    """
-        This method is used to create the users for the specified merchant
-        This calls the generate_user_creation_api_body method for getting the list of request bodies.
-        Each request body represents a user.
-        """
-    api_details = get_api_from_db("createUser")
-    conn = sqlite3.connect(dbPath)
-    cursor = conn.cursor()
-    url = ConfigReader.read_config("APIs", "baseurl") + api_details["EndPoint"]
-    headers = json.loads(api_details["Header"])
-    global user_name
-    lst_user_creation_body = generate_users_creation_api_body(merchant_code)
-    if lst_user_creation_body:
-        for user_creation_api_body in lst_user_creation_body:
-            user_name = user_creation_api_body["name"]
-            payload = json.dumps(user_creation_api_body)
-            logger.debug(payload)
-            response = requests.request("POST", url, headers=headers, data=payload)
-            logger.debug(response.text)
-            result = json.loads(response.text)
-            if result["success"]:
-                logger.info(f"User {user_name} with users created successfully.")
-                try:
-                    cursor.execute(
-                        "update users set CreationStatus = 'Created', Availability = 'Available' where Name = '" + user_name + "'")
-                    conn.commit()
-                except Exception as e:
-                    print(
-                        f"Unable to update the ezeauto db on the user {user_name} creation due to error " + str(
-                            e))
-                    logger.warning(
-                        f"Unable to update the ezeauto db on the user {user_name} creation due to error " + str(
-                            e))
-            else:
-                logger.warning(f"User {user_name} creation failed")
 
-    else:
-        print("User creation skipped.")
-        logger.info("User creation skipped")
+def update_merchants_db(merchants_list : list):
+    conn = ""
+    cursor = ""
+    try:
+        conn = sqlite3.connect(dbPath)
+        cursor = conn.cursor()
+        for merchant in merchants_list:
+            try:
+                cursor.execute(f"insert into merchants(MerchantCode)values('{merchant}');")
+                conn.commit()
+                logger.info(f"Merchant {merchant} successfully added to the merchants db.")
+            except Exception as e:
+                logger.error(f"Unable to add the merchant {merchant} into db due to error {e}")
+    except Exception as e:
+        logger.error(f"Unable to connect to the db due to error {e}")
     cursor.close()
     conn.close()
+
+def update_users_db(users_list : list):
+    conn = ""
+    cursor = ""
+    try:
+        conn = sqlite3.connect(dbPath)
+        cursor = conn.cursor()
+        for user in users_list:
+            if(check_merchant_exists_in_automation_db(user["MerchantCode"])):
+                try:
+                    cursor.execute(f"insert into users(Name, MerchantCode, Username, Password, Mobile, Type)values(\"{user['Name']}\", \"{user['MerchantCode']}\", \"{user['Username']}\", \"{user['Password']}\", \"{user['Mobile']}\", \"{user['Type']}\");")
+                    conn.commit()
+                    logger.info(f"User {user['Name']} successfully added to the users db.")
+                except Exception as e:
+                    logger.error(f"Unable to add the user {user['Name']} into db due to error {e}")
+            else:
+                logger.info(f"User {user['Name']} creation skipped since associated merchant is not available in merchants db.")
+    except Exception as e:
+        logger.error(f"Unable to connect to the db due to error {e}")
+    cursor.close()
+    conn.close()
+
+
+def check_merchant_exists_in_automation_db(merchant):
+    conn = ""
+    cursor = ""
+    exists = ""
+    try:
+        conn = sqlite3.connect(dbPath)
+        cursor = conn.cursor()
+        try:
+            cursor.execute(f"select * from merchants where MerchantCode = '{merchant}'")
+            merchant_info = cursor.fetchone()
+            if merchant_info is None:
+                logger.info(f"Merchant {merchant} is not available in the automation db.")
+                exists =  False
+            else:
+                logger.info(f"Merchant {merchant} is available in the automation db.")
+                exists =  True
+        except Exception as e:
+            logger.error(f"Unable to check if {merchant} is available in the automation db.")
+    except Exception as e:
+        logger.error(f"Unable to connect to the db due to error {e}")
+    cursor.close()
+    conn.close()
+    return exists
+
+
+def get_merchants_list_from_excel(merchant_creation_excel_path) -> list:
+    merchants_list = []
+    merchant_creation_excel_data = pandas.read_excel(merchant_creation_excel_path, sheet_name="UserDetails")
+    if len(merchant_creation_excel_data)>0:
+        for i in range(0, len(merchant_creation_excel_data)):
+            merchant_name = ""
+            if str((merchant_creation_excel_data.loc[i]).to_dict()["Type"]).lower() == "portal":
+                merchant_name = "EZETAP"
+            elif str((merchant_creation_excel_data.loc[i]).to_dict()["Type"]).lower() in ["admin", "app"]:
+                merchant_name = (merchant_creation_excel_data.loc[i]).to_dict()["MerchantCode"]
+            else:
+                merchant_name = (merchant_creation_excel_data.loc[i]).to_dict()["MerchantCode"]
+                logger.error(f"Merchant {merchant_name} is not added to the list since type is specified wrongly.")
+                merchant_name = ""
+            if not merchant_name in merchants_list and merchant_name != "":
+                merchants_list.append(merchant_name)
+    else:
+        logger.warning("Unable to pull merchants list since no data available in the merchant creation excel file.")
+    return merchants_list
+
+
+def get_users_list_from_excel(merchant_creation_excel_path) -> list:
+    users_list = []
+    merchant_creation_excel_data = pandas.read_excel(merchant_creation_excel_path, sheet_name="UserDetails")
+    if len(merchant_creation_excel_data) > 0:
+        for i in range(0, len(merchant_creation_excel_data)):
+            if str((merchant_creation_excel_data.loc[i]).to_dict()["Type"]).lower() in ["portal", "admin", "app"]:
+                user_details = {}
+                user_details["Name"] = (merchant_creation_excel_data.loc[i]).to_dict()["Name"]
+                if str((merchant_creation_excel_data.loc[i]).to_dict()["Type"]).lower() == "portal":
+                    user_details["MerchantCode"] = "EZETAP"
+                else:
+                    user_details["MerchantCode"] = (merchant_creation_excel_data.loc[i]).to_dict()["MerchantCode"]
+                user_details["Username"] = (merchant_creation_excel_data.loc[i]).to_dict()["Username"]
+                user_details["Password"] = (merchant_creation_excel_data.loc[i]).to_dict()["Password"]
+                user_details["Mobile"] = (merchant_creation_excel_data.loc[i]).to_dict()["Mobile"]
+                user_details["Type"] = (merchant_creation_excel_data.loc[i]).to_dict()["Type"]
+                users_list.append(user_details)
+            else:
+                logger.error(f"Type of user {(merchant_creation_excel_data.loc[i]).to_dict()['Name']} is defined wrongly. So its not added to user creation list.")
+    else:
+        logger.warning("Unable to pull users list since no data available in the merchant creation excel file.")
+    return users_list
