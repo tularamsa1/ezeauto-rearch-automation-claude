66bb39e0f26843b574045ba1923e38cc77012904
nivinselvam <nivin.p@ezetap.com>
2022-07-19 12:37:16 +0530

    Loggers updated for merchant creation

--- Utilities/ResourceAssigner.py
+++ Utilities/ResourceAssigner.py
@@ -1,4 +1,3 @@
-#ResourceAssigner
 import sqlite3
 import time
 from datetime import datetime
@@ -68,7 +67,6 @@ def getDeviceFromDB(testCaseID):
         return None
     else:
         return device
-
 
 
 def releaseDeviceInDBusingTestCaseID(testCaseID):
