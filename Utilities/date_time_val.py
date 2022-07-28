ae73d9b329f984ef6f136fd527bfb102ac090473
AvinashChoudhary <avinash.c@ezetap.com>
2022-07-28 12:27:33 +0530

    date and time validation incorported for one upi test case and added date_and_time utility

---
+++ Utilities/date_time_val.py
@@ -0,0 +1,45 @@
+from datetime import datetime, timedelta
+import pytz
+from Utilities.execution_log_processor import EzeAutoLogger
+
+logger = EzeAutoLogger(__name__)
+
+
+def date_and_time_val_against_app(posting_date_db):
+    date_format = "%Y-%m-%dT%H:%M:%S.%f"
+    app_format = "%d %b %Y, %I:%M %p"
+    dt_str1 = datetime.strptime(str(posting_date_db), '%Y-%m-%dT%H:%M:%S.%f000').strftime("%Y-%m-%dT%H:%M:%S.%f")
+    dt_utc = datetime.strptime(dt_str1, date_format)
+    utc_date = dt_utc.replace(tzinfo=pytz.UTC)
+    now_asia = utc_date.astimezone(pytz.timezone('Asia/Kolkata'))
+    datetime_in_app_format = now_asia.strftime(app_format)
+    return datetime_in_app_format
+
+
+def date_and_time_val_against_api(api_datetime_in_ms):
+    my_datetime = datetime.fromtimestamp(int(api_datetime_in_ms) / 1000)  # Apply from timestamp function
+    dt_str1 = my_datetime - timedelta(hours=11, minutes=0)
+    db_date_time = datetime.strptime(str(dt_str1), '%Y-%m-%d %H:%M:%S').strftime("%Y-%m-%d %H:%M:%S")
+    return db_date_time
+
+
+def db_datetime(date_from_db):
+    format1 = "%Y-%m-%dT%H:%M:%S.%f"
+    date_format = "%Y-%m-%d %H:%M:%S"
+    dt_str = date_from_db
+    dt_str1 = datetime.strptime(str(dt_str), '%Y-%m-%dT%H:%M:%S.%f000').strftime("%Y-%m-%dT%H:%M:%S.%f")
+    dt_utc = datetime.strptime(dt_str1, format1).strftime(date_format)
+    return dt_utc
+
+
+def date_and_time_val_against_charge_slip(posting_date_db):
+    date_format = "%Y-%m-%dT%H:%M:%S.%f"
+    format1 = "%Y-%m-%d, %H:%M:%S"
+    dt_str = posting_date_db
+    dt_str1 = datetime.strptime(str(dt_str), '%Y-%m-%dT%H:%M:%S.%f000').strftime("%Y-%m-%dT%H:%M:%S.%f")
+    dt_utc = datetime.strptime(dt_str1, date_format)
+    utc_date = dt_utc.replace(tzinfo=pytz.UTC)
+    now_asia = utc_date.astimezone(pytz.timezone('Asia/Kolkata')).strftime(format1)
+    date_from_db = now_asia.split(",")[0]
+    time_from_db = now_asia.split(",")[1].lstrip()
+    return date_from_db, time_from_db
