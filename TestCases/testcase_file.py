e29e3ef4b2378da34fa81364c63f1e18f6ca9a4a
nivinselvam <nivin.p@ezetap.com>
2022-08-11 08:16:17 +0530

    Configuration for installing apps

--- TestCases/testcase_file.py
+++ TestCases/testcase_file.py
@@ -1,66 +1,40 @@
-import time
+import json
+from appium import webdriver as app_webdriver
 
-import pytest
+from Utilities import ConfigReader
 
-from Utilities import ResourceAssigner
 
-def test_case01():
-    appuser = ResourceAssigner.getAppUserCredentials("test_case01")
-    portaluser = ResourceAssigner.getPortalUserCredentials("test_case01")
-    if appuser and portaluser:
-        print(f"TC 01 - {appuser}")
-        print(f"TC 01 - {portaluser}")
+def start_app():
+    device_details = {'DeviceId': 'emulator-5554'}
+    appium_server_details = {'PortNumber':'4723'}
+    print(" will be using the device " + device_details['DeviceId'])
+    print(" will be running on the appium server port " + appium_server_details['PortNumber'])
+    if str(ConfigReader.read_config("Applications", "install_apps")).lower() == 'true':
+        mpos_app = "/home/ezetap-10182/Downloads/EzeAuto/App/MPOSX_10.0.100.apk"
+        # mpos_app = "/home/ezetap-10182/Desktop/Nivin/Documents/APKs/MPOS/ezetap_android.apk"
+        sa_app = "/home/ezetap-10182/Downloads/EzeAuto/App/SA_10.16.12.apk"
+        lst_applications = [mpos_app, sa_app]
+        json_applications = json.dumps(lst_applications)
     else:
-        pytest.fail()
-    time.sleep(5)
-    print(ResourceAssigner.releaseAppUser("test_case01"))
-    print(ResourceAssigner.releasePortalUser("test_case01"))
+        json_applications = ""
+    desired_cap = {
+        "platformName": "Android",
+        "deviceName": device_details['DeviceId'],
+        "udid": device_details['DeviceId'],
+        "otherApps": json_applications,
+        "appPackage": "com.ezetap.basicapp",
+        "appActivity": "com.ezetap.mposX.activity.SplashActivity",
+        "ignoreHiddenApiPolicyError": "true",
+        "noReset": "false",
+        "autoGrantPermissions": "true",
+        "newCommandTimeout": 7000,
+        "MobileCapabilityType.AUTOMATION_NAME": "AutomationName.ANDROID_UIAUTOMATOR2",
+        "MobileCapabilityType.NEW_COMMAND_TIMEOUT": "300"
+    }
+    print("appium server url:", 'http://127.0.0.1:' + appium_server_details['PortNumber'] + '/wd/hub')
+    appDriver = app_webdriver.Remote(
+        'http://127.0.0.1:' + appium_server_details['PortNumber'] + '/wd/hub', desired_cap)
+    print("installation completed")
 
-def test_case02():
-    appuser = ResourceAssigner.getAppUserCredentials("test_case02")
-    portaluser = ResourceAssigner.getPortalUserCredentials("test_case02")
-    if appuser and portaluser:
-        print(f"TC 02 - {appuser}")
-        print(f"TC 02 - {portaluser}")
-    else:
-        pytest.fail()
-    time.sleep(5)
-    print(ResourceAssigner.releaseAppUser("test_case02"))
-    print(ResourceAssigner.releasePortalUser("test_case02"))
 
-def test_case03():
-    appuser = ResourceAssigner.getAppUserCredentials("test_case03")
-    portaluser = ResourceAssigner.getPortalUserCredentials("test_case03")
-    if appuser and portaluser:
-        print(f"TC 03 - {appuser}")
-        print(f"TC 03 - {portaluser}")
-    else:
-        pytest.fail()
-    time.sleep(2)
-    print(ResourceAssigner.releaseAppUser("test_case03"))
-    print(ResourceAssigner.releasePortalUser("test_case03"))
-
-def test_case04():
-    appuser = ResourceAssigner.getAppUserCredentials("test_case04")
-    portaluser = ResourceAssigner.getPortalUserCredentials("test_case04")
-    if appuser and portaluser:
-        print(f"TC 04 - {appuser}")
-        print(f"TC 04 - {portaluser}")
-    else:
-        pytest.fail()
-    time.sleep(2)
-    print(ResourceAssigner.releaseAppUser("test_case04"))
-    print(ResourceAssigner.releasePortalUser("test_case04"))
-
-def test_case05():
-    appuser = ResourceAssigner.getAppUserCredentials("test_case05")
-    portaluser = ResourceAssigner.getPortalUserCredentials("test_case05")
-    if appuser and portaluser:
-        print(f"TC 05 - {appuser}")
-        print(f"TC 05 - {portaluser}")
-    else:
-        pytest.fail()
-    time.sleep(2)
-    print(ResourceAssigner.releaseAppUser("test_case05"))
-    print(ResourceAssigner.releasePortalUser("test_case05"))
-
+start_app()
