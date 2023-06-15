7bd2613755ff43aa604ecc21470b67795f1ec438
divya <divya.andrews@razorpay.com>
2023-06-15 16:16:51 +0530

    Reverted back portal val of P2P

--- TestCases/Functional/UI/Common/P2P/test_UI_Common_P2P_BQR_Start_API_01.py
+++ TestCases/Functional/UI/Common/P2P/test_UI_Common_P2P_BQR_Start_API_01.py
@@ -2060,7 +2060,6 @@ def test_common_500_502_010():
 @pytest.mark.usefixtures("log_on_success", "method_setup")
 @pytest.mark.apiVal
 @pytest.mark.dbVal
-@pytest.mark.portalVal
 def test_common_500_502_036():
     """
     Sub Feature Code: UI_Common_P2P_BQR_Cancel_API
@@ -2149,14 +2148,12 @@ def test_common_500_502_036():
         logger.info(f"from bharatqr_merchant_config, terminal_info_id is : {db_bqr_terminal_info_id}")
         logger.info(f"from bharatqr_merchant_config, merchant_pan is : {db_bqr_config_merchant_pan}")
 
-        TestSuiteSetup.launch_browser_and_context_initialize()
-
         GlobalVariables.setupCompletedSuccessfully = True
         logger.info(f"Completed Precondition setup for the test case : {testcase_id}")
         # -----------------------------PreConditions(Completed)-----------------------------
 
         # Set the below variables depending on the log capturing need of the test case.
-        Configuration.configureLogCaptureVariables(apiLog=True, commx_log=True, portalLog=True)
+        Configuration.configureLogCaptureVariables(apiLog=True, commx_log=True)
 
         GlobalVariables.time_calc.setup.end()
         logger.debug(f"Setup Timer ended in testcase function : {testcase_id}")
@@ -2408,57 +2405,6 @@ def test_common_500_502_036():
                 Configuration.perform_db_val_exception(testcase_id, e)
             logger.info(f"Completed DB validation for the test case : {testcase_id}")
         # -----------------------------------------End of DB Validation---------------------------------------
-        # -----------------------------------------Start of Portal Validation---------------------------------
-        if (ConfigReader.read_config("Validations", "portal_validation")) == "True":
-            logger.info(f"Started PORTAL validation for the test case : {testcase_id}")
-            try:
-                date_and_time_portal = date_time_converter.to_portal_format(txn_created_time)
-                expected_portal_values = {
-                    "date_time": date_and_time_portal,
-                    "pmt_state": "EXPIRED",
-                    "pmt_type": "BHARATQR",
-                    "txn_amt": f"{str(amount)}.00",
-                    "username": app_username,
-                    "txn_id": txn_id,
-                    "rrn": "RRN",
-                    "acct_label": "OTA",
-                    "auth_code": "auth_code"
-                }
-                logger.debug(f"expected_portal_values : {expected_portal_values}")
-
-                transaction_details = get_transaction_details_for_portal(app_username, app_password,
-                                                                         ext_ref_number)
-                date_time = transaction_details[0]['Date & Time']
-                transaction_id = transaction_details[0]['Transaction ID']
-                total_amount = transaction_details[0]['Total Amount'].split()
-                rr_number = transaction_details[0]['RR Number']
-                transaction_type = transaction_details[0]['Type']
-                status = transaction_details[0]['Status']
-                username = transaction_details[0]['Username']
-                labels = transaction_details[0]['Labels']
-                auth_code_portal = transaction_details[0]['Auth Code']
-
-                actual_portal_values = {
-                    "date_time": date_time,
-                    "pmt_state": str(status),
-                    "pmt_type": transaction_type,
-                    "txn_amt": total_amount[1],
-                    "username": username,
-                    "txn_id": transaction_id,
-                    "rrn": rr_number,
-                    "acct_label": labels,
-                    "auth_code": auth_code_portal
-                }
-
-                logger.debug(f"actual_portal_values : {actual_portal_values}")
-
-                Validator.validateAgainstPortal(expectedPortal=expected_portal_values,
-                                                actualPortal=actual_portal_values)
-            except Exception as e:
-                Configuration.perform_portal_val_exception(testcase_id, e)
-            logger.info(f"Completed Portal validation for the test case : {testcase_id}")
-        # -----------------------------------------End of Portal Validation---------------------------------------
-
         GlobalVariables.time_calc.validation.end()
         logger.debug(f"Validation Timer ended in testcase function : {testcase_id}")
         logger.info(f"Completed Validation for the test case : {testcase_id}")
