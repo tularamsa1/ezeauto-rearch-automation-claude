60e7108d361414f3c53cfb47b16aab68fd4f134a
swathit <swathi.t@ezetap.com>
2023-07-14 11:12:47 +0530

    Script adaptations - rc_14_07_2023

--- TestCases/Functional/UI/Common/test_UI_Common_BQR_Refund_HDFC.py
+++ TestCases/Functional/UI/Common/test_UI_Common_BQR_Refund_HDFC.py
@@ -1984,7 +1984,7 @@ def test_common_100_102_084():
                     "settle_status_2": "REVPENDING",
                     "txn_id_2": txn_id_new_2,
                     "order_id_2": order_id,
-                    "pmt_msg_2": "PAYMENT SUCCESSFUL",
+                    "pmt_msg_2": "REFUND PENDING",
                     "date_2": date_and_time_new_2
                 }
                 logger.debug(f"expectedAppValues: {expected_app_values}")
