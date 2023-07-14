63096524b8a7ac5289a586596c36953570eec611
vishrutsharma <vishrut.sharma@razorpay.com>
2023-07-14 18:39:51 +0530

    script adaptations - rc_14_07_2023

--- TestCases/Functional/UI/Common/UPI/test_UI_Common_PM_UPI_AXIS_DIRECT_Refund_TID_Dep_02.py
+++ TestCases/Functional/UI/Common/UPI/test_UI_Common_PM_UPI_AXIS_DIRECT_Refund_TID_Dep_02.py
@@ -253,7 +253,7 @@ def test_common_100_101_189():
                     "order_id": order_id,
                     "order_id_2": order_id,
                     "pmt_msg": "PAYMENT SUCCESSFUL",
-                    "pmt_msg_2": "PAYMENT SUCCESSFUL",
+                    "pmt_msg_2": "REFUND PENDING",
                     "rrn": str(rrn),
                     "date": date_and_time,
                     "date_2": refund_date_and_time
--- TestCases/Functional/UI/Common/UPI/test_UI_Common_PM_UPI_AXIS_DIRECT_UPG_TID_Dep_01.py
+++ TestCases/Functional/UI/Common/UPI/test_UI_Common_PM_UPI_AXIS_DIRECT_UPG_TID_Dep_01.py
@@ -228,7 +228,7 @@ def test_common_100_101_206():
                     "settle_status": "SETTLED",
                     "txn_id": ipr_txn_id,
                     "rrn": str(rrn),
-                    "order_id": external_ref,
+                    # "order_id": external_ref,
                     "pmt_msg": "PAYMENT SUCCESSFUL",
                     "date": date_and_time,
 
@@ -271,8 +271,8 @@ def test_common_100_101_206():
                     f"Fetching txn settlement_status from txn history for the txn : {ipr_txn_id}, {app_settlement_status}")
                 app_payment_msg = txn_history_page.fetch_txn_payment_msg_text()
                 logger.info(f"Fetching txn status msg from txn history for the txn : {ipr_txn_id}, {app_payment_msg}")
-                app_order_id = txn_history_page.fetch_order_id_text()
-                logger.info(f"Fetching txn order_id from txn history for the txn : {ipr_txn_id}, {app_order_id}")
+                # app_order_id = txn_history_page.fetch_order_id_text()
+                # logger.info(f"Fetching txn order_id from txn history for the txn : {ipr_txn_id}, {app_order_id}")
                 app_rrn = txn_history_page.fetch_RRN_text()
                 logger.info(
                     f"Fetching txn_id from txn history for the txn : {ipr_txn_id}, {app_rrn}")
@@ -304,7 +304,7 @@ def test_common_100_101_206():
                     "settle_status": app_settlement_status,
                     "txn_id": app_txn_id,
                     "rrn": str(app_rrn),
-                    "order_id": app_order_id,
+                    # "order_id": app_order_id,
                     "pmt_msg": app_payment_msg,
                     "date": app_date_and_time,
 
